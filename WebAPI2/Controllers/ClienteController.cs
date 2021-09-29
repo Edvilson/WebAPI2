256f2bcfbac059b9fe84279c01180f521f94136e
Edvilson <edvilson.ads@gmail.com>
2021-09-28 22:03:54 -0300

    add project

---
+++ WebAPI2/Controllers/ClienteController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI2.Models;
+
+namespace WebAPI2.Controllers
+{
+    public class ClienteController : ApiController
+    {
+
+        private List<Cliente> clientes = new List<Cliente>()
+        {
+            new Cliente(){ cod_cliente=1, nome_cliente="Angélica Noronha", telefone_cliente="585968541"},
+            new Cliente(){ cod_cliente=2, nome_cliente="Edvilson Almeida", telefone_cliente="369258147"},
+            new Cliente(){ cod_cliente=3, nome_cliente="Burgos Filho", telefone_cliente="147258963"}
+        };
+
+        public IEnumerable<Cliente> Get()
+        {
+            return clientes.ToList();
+        }
+
+        public Cliente Get(int id)
+        {
+
+            return clientes.FirstOrDefault(x => x.cod_cliente == id);
+
+        }
+    }
+}
---
+++ WebAPI2/Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI2.Models
+{
+    public class Cliente
+    {
+        public int cod_cliente { get; set; }
+        public string nome_cliente { get; set; }
+        public string telefone_cliente { get; set; }
+
+    }
+}
